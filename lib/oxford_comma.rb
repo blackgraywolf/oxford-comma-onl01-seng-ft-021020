af568d44eb6e06180e6adce80e77112abbd6cde2
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-13 01:29:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  array = ["1","2","3","4"]
+  ##array = ["1","2","3","4"]
  save = " and " + array.last
  array.pop
  
- new_array = array.join(" ") + "," << save
+ new_array = array.join(", ") + "" << save
  return new_array
 end
